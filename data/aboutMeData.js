9cf7c813f03e3f6b3831c8402b534f180bbe9469
keertana-chinni <keerthana.trasula@gmail.com>
2025-05-07 16:32:20 +0530

    Update aboutMeData.js

--- data/aboutMeData.js
+++ data/aboutMeData.js
@@ -3,10 +3,10 @@ import { v4 as uuidv4 } from 'uuid';
 export const aboutMeData = [
 	{
 		id: uuidv4(),
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+		bio: 'I’m an enthusiastic and self-motivated full-stack development beginner currently building a strong foundation in web development. I am actively learning and working on projects that involve modern front-end technologies such as HTML, CSS, JavaScript, React, and Next.js. My focus is on creating responsive and user-friendly interfaces while understanding the core principles of design and performance optimization. As part of my learning journey, I am also exploring tools and workflows like npm and version control using Git to follow industry best practices.',
 	},
 	{
 		id: uuidv4(),
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-	},
+		bio: 'On the backend side, I have hands-on knowledge of Node.js for server-side development and am familiar with integrating relational databases such as MySQL. My aim is to become a well-rounded full-stack developer by continuously practicing and building real-world applications that connect both front-end and back-end seamlessly. I’m passionate about learning, problem-solving, and contributing to team-based projects where I can apply and grow my skills in real development environments.'
+        },
 ];
